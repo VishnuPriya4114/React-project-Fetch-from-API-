01ea28c58e1df6ce35c969b78f782198f7597eed
Vishnu_Priya-SKCET <123156204+VishnuPriya4114@users.noreply.github.com>
2023-12-25 08:55:08 +0530

    Add files via upload
    A react component that fetch data from API and store it in a table

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setLoading(true)
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(json => setUsers(json))
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          <h1 className="user">Users</h1>
+          <table border={1} className="t">
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+              </tr>
+            ))}
+          </table>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default App
